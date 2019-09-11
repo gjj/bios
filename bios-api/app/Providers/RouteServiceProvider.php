0f9f7ac339f98fd6dd7ea7b904009a13a0006da1
Goi Jia Jian <jiajian.goi.2018@sis.smu.edu.sg>
2019-09-11 13:04:58 +0800

    Updated API route prefix to root.

--- bios-api/app/Providers/RouteServiceProvider.php
+++ bios-api/app/Providers/RouteServiceProvider.php
@@ -65,7 +65,7 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapApiRoutes()
     {
-        Route::prefix('app/json')
+        Route::prefix('')
              ->middleware('api')
              ->namespace($this->namespace)
              ->group(base_path('routes/api.php'));
