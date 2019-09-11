3d04fd36eeb10ffceef95537982cfddca95f7e3a
Goi Jia Jian <jiajian.goi.2018@sis.smu.edu.sg>
2019-09-11 11:19:38 +0800

    test

--- bios-web/home.php
+++ bios-web/home.php
@@ -9,6 +9,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+	<base href="">
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -21,8 +22,8 @@
 	<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
 	<!-- Custom styles for this template-->
-	<link href="assets/css/sb-admin-2.min.css" rel="stylesheet">
-
+	<link href="assets/css/bios.css" rel="stylesheet">
+	<link href="assets/css/fontawesome.min.css" rel="stylesheet">
 </head>
 
 <body>
@@ -35,9 +36,6 @@
 
 			<!-- Sidebar - Brand -->
 			<a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-				<div class="sidebar-brand-icon rotate-n-15">
-					<i class="fas fa-laugh-wink"></i>
-				</div>
 				<div class="sidebar-brand-text mx-3">MerlionU BIOS</div>
 			</a>
 
@@ -47,79 +45,37 @@
 			<!-- Nav Item - Dashboard -->
 			<li class="nav-item active">
 				<a class="nav-link" href="index.html">
-					<i class="fas fa-fw fa-tachometer-alt"></i>
-					<span>Dashboard</span></a>
+					<span>Dashboard</span>
+				</a>
 			</li>
 
-			<!-- Divider -->
-			<hr class="sidebar-divider">
+			<hr class="sidebar-divider" />
 
-			<!-- Heading -->
 			<div class="sidebar-heading">
 				Interface
 			</div>
 
-			<!-- Nav Item - Pages Collapse Menu -->
 			<li class="nav-item">
-				<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-					<i class="fas fa-fw fa-cog"></i>
+				<a class="nav-link" href="#">
 					<span>Components</span>
 				</a>
-				<div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-					<div class="bg-white py-2 collapse-inner rounded">
-						<h6 class="collapse-header">Custom Components:</h6>
-						<a class="collapse-item" href="buttons.html">Buttons</a>
-						<a class="collapse-item" href="cards.html">Cards</a>
-					</div>
-				</div>
 			</li>
 
-			<!-- Nav Item - Utilities Collapse Menu -->
 			<li class="nav-item">
-				<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
-					<i class="fas fa-fw fa-wrench"></i>
+				<a class="nav-link" href="#">
 					<span>Utilities</span>
 				</a>
-				<div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
-					<div class="bg-white py-2 collapse-inner rounded">
-						<h6 class="collapse-header">Custom Utilities:</h6>
-						<a class="collapse-item" href="utilities-color.html">Colors</a>
-						<a class="collapse-item" href="utilities-border.html">Borders</a>
-						<a class="collapse-item" href="utilities-animation.html">Animations</a>
-						<a class="collapse-item" href="utilities-other.html">Other</a>
-					</div>
-				</div>
 			</li>
 
 		</ul>
-		<!-- End of Sidebar -->
 
-		<!-- Content Wrapper -->
 		<div id="content-wrapper" class="d-flex flex-column">
-
-			<!-- Main Content -->
 			<div id="content">
-
-				<!-- Topbar -->
 				<nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-
-					<!-- Sidebar Toggle (Topbar) -->
 					<button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-						<i class="fa fa-bars"></i>
+						<i class="fas fa-bars"></i>
 					</button>
-
-					<!-- Topbar Search -->
-					<form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-						<div class="input-group">
-							<input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
-							<div class="input-group-append">
-								<button class="btn btn-primary" type="button">
-									<i class="fas fa-search fa-sm"></i>
-								</button>
-							</div>
-						</div>
-					</form>
-
+					
 					<!-- Topbar Navbar -->
 					<ul class="navbar-nav ml-auto">
 
