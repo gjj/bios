13d5f6286d6c4952b7787ba450b558b0005ad4e1
Goi Jia Jian <jiajian.goi.2018@sis.smu.edu.sg>
2019-09-15 02:23:55 +0800

    test

--- includes/views/header.php
+++ includes/views/header.php
@@ -22,8 +22,24 @@
 		
 	</head>
 	<body>
-		<nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+		<nav class="navbar navbar-expand-sm navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
 			<a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">BIOS</a>
+
+			<!--<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+
+			<div class="collapse navbar-collapse d-xl-none" id="navbarsExample03">
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item active">
+						<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Course Offerings</a>
+					</li>
+				</ul>
+			</div>-->
+			
 			<ul class="navbar-nav px-3">
 				<li class="nav-item text-nowrap">
 					<a class="nav-link" href="logout">Sign out</a>
