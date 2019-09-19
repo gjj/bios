e36508539629a7126b1cb1922a41a2e518528dc7
Goi Jia Jian <jiajian.goi.2018@sis.smu.edu.sg>
2019-09-19 17:17:47 +0800

    ok

--- cart.php
+++ cart.php
@@ -60,14 +60,87 @@
         </div>
 
         <div class="row pb-5">
-            
 			<div class="col-md-12">
                 <h5>My Cart</h5>
+
+                ***REMOVED***
+								if (isset($_SESSION['errors'])) {
+							?>
+							<div class="alert alert-danger alert-dismissible fade show" role="alert">
+								***REMOVED***
+									printErrors();
+								?>
+								
+								<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+									<span aria-hidden="true">&times;</span>
+								</button>
+							</div>
+
+							***REMOVED***
+								}
+							?>
+
+                <section>
+                    <form action="cart_checkout" method="post">
+                        <table class="table">
+                            <thead class="thead-dark">
+                                <tr>
+                                    <th scope="col"></th>
+                                    <th scope="col">Course Code</th>
+                                    <th scope="col">Section</th>
+                                    <th scope="col">Day</th>
+                                    <th scope="col">Start</th>
+                                    <th scope="col">End</th>
+                                    <th scope="col">Instructor</th>
+                                    <th scope="col">Venue</th>
+                                    <th scope="col">Size</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                ***REMOVED***
+                                    $bids = $bidDAO->retrieveAllCartItemsByUser($user['userid'], $currentRound['round']);
+                                    
+                                    $i = 0;
+                                    foreach ($bids as $bid) {
+                                        
+                                ?>
+                                <tr>
+                                    <!-- This is just one way of passing data over, using hidden fields. Because our design is like that... -->
+                                    <td>
+                                        <input type="checkbox" name="checkout[]" value="***REMOVED*** echo $i; ?>" />
+                                    </td>
+                                    <td>***REMOVED*** echo $bid['code'];?><input type="hidden" name="code[]" value="***REMOVED*** echo $bid['code'];?>" /></td>
+                                    <td>***REMOVED*** echo $bid['section'];?><input type="hidden" name="section[]" value="***REMOVED*** echo $bid['section'];?>" /></td>
+                                    <td>***REMOVED*** echo $bid['day'];?></td>
+                                    <td>***REMOVED*** echo $bid['start'];?></td>
+                                    <td>***REMOVED*** echo $bid['end'];?></td>
+                                    <td>***REMOVED*** echo $bid['instructor'];?></td>
+                                    <td>***REMOVED*** echo $bid['venue'];?></td>
+                                    <td>***REMOVED*** echo $bid['size'];?></td>
+                                </tr>
+                                ***REMOVED***
+                                        $i++;
+                                ***REMOVED***
+                                ?>
+                            </tbody>
+                        </table>
+                        <p>
+                            <button type="submit" class="btn btn-info">Checkout</button>
+                        </p>
+                    </form>
+				</section>
+            </div>
+        </div>
+
+        <div class="row pb-5">
+            <div class="col-md-12">
+                <h5>My Bids</h5>
                 <section>
 					<table class="table">
 						<thead class="thead-dark">
 							<tr>
-							    <th scope="col">Checkout</th>
+							    <th scope="col"></th>
+                                <th scope="col">Bid (e$)</th>
                                 <th scope="col">Course Code</th>
                                 <th scope="col">Section</th>
 							    <th scope="col">Day</th>
@@ -79,32 +152,34 @@
 							</tr>
 						</thead>
 						<tbody>
-                            ***REMOVED***
-                                $bids = $bidDAO->retrieveAllBidsByUser($user['userid'], $currentRound['round']);
-                                
-                                foreach ($bids as $bid) {
-                            ?>
-							<tr>
-                                <td>
-                                    <input type="checkbox" name="checkout[]" />
-                                </td>
-                                <td>***REMOVED*** echo $bid['code'];?></td>
-							    <td>***REMOVED*** echo $bid['section'];?></td>
-							    <td>***REMOVED*** echo $bid['day'];?></td>
-							    <td>***REMOVED*** echo $bid['start'];?></td>
-							    <td>***REMOVED*** echo $bid['end'];?></td>
-							    <td>***REMOVED*** echo $bid['instructor'];?></td>
-							    <td>***REMOVED*** echo $bid['venue'];?></td>
-							    <td>***REMOVED*** echo $bid['size'];?></td>
-							</tr>
-                            ***REMOVED***
-                            ***REMOVED***
-                            ?>
-						</tbody>
-					</table>
-				</section>
+                                ***REMOVED***
+                                    $bids = $bidDAO->retrieveAllBidsByUser($user['userid'], $currentRound['round']);
+                                    
+                                    foreach ($bids as $bid) {
+                                ?>
+                                <tr>
+                                    <td>
+                                        <input type="checkbox" class="input" name="checkout[]" />
+                                    </td>
+                                    <td>***REMOVED*** echo $bid['amount'];?></td>
+                                    <td>***REMOVED*** echo $bid['code'];?></td>
+                                    <td>***REMOVED*** echo $bid['section'];?></td>
+                                    <td>***REMOVED*** echo $bid['day'];?></td>
+                                    <td>***REMOVED*** echo $bid['start'];?></td>
+                                    <td>***REMOVED*** echo $bid['end'];?></td>
+                                    <td>***REMOVED*** echo $bid['instructor'];?></td>
+                                    <td>***REMOVED*** echo $bid['venue'];?></td>
+                                    <td>***REMOVED*** echo $bid['size'];?></td>
+                                </tr>
+                                ***REMOVED***
+                                ***REMOVED***
+                                ?>
+                        </tbody>
+                    </table>
+                </section>
             </div>
         </div>
+
         <div class="row">
             <div class="col-md-12">
             <h5>My Calendar</h5>
