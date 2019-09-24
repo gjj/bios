91576529ec85649617aae67e1d8799f1fd2e9dd5
Goi Jia Jian <jiajian.goi.2018@sis.smu.edu.sg>
2019-09-25 01:20:42 +0800

    ok

--- cart_checkout.php
+++ cart_checkout.php
@@ -37,6 +37,8 @@
             foreach ($bids as $bid) {
                 $bidDAO->addBid($user['userid'], $bid['course'], $bid['section'], $bid['amount'], $currentRound['round']);
             }
+
+            header("Location: cart");
         }
     }
     include 'includes/views/header.php';
