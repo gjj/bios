d5e08c258ae45e0e5bea252505c65818d626d494
alice <mesin.2017@sis.smu.edu.sg>
2019-09-28 00:59:45 +0800

    Student.csv file specific validations

--- admin/bootstrap-validation.php
+++ admin/bootstrap-validation.php
@@ -1,40 +1,115 @@
 <?php
 
 require_once '../includes/common.php';
-// commonValidation to check each file, if there's a row with any of the column data missing, discard that row
-// incomplete
-function hasEmptyField(array $data)
-{
-    for ($i = 0; $i <=len($data); $i++) {
+
+// $data is an array of each row values
+function hasEmptyField($data){
+    $columnpos_arr = [];
+    for ($i = 0; $i <=count($data); $i++) {
         // Make sure that the key exists, isn't null or an empty string
-        if (!isset($data[$i]) || $data[$i] === '') {
-            
-            return $i;
+        if (!isset($data[$i]) || $data[$i] == '') {
+            $columnpos_arr[] = $i;
         }
     }
 
-    return false;
+    return $columnpos_arr; // position of columns with missing values 
 }
 // incomplete
-function commonValidation($file){
+// function commonValidation($file){
 
-    $counter = 0;
+//     $counter = 0;
+//     $errors= [];
+//     while (($data = fgetcsv($file, 1000, ",")) !== FALSE) {        
+//         $counter++;    
+//         $columnpos_arr = hasEmptyField($data);
+//         if (!empty($columnpos_arr)) {
+//             foreach($columnpos as $columnpos_arr){
 
-    while (($data = fgetcsv($file, 1000, ",")) !== FALSE) {        
+//             }
+//              $errors[] = "Empty field is in row: $counter cell $column_error";
+//         }
 
-        $counter++;    
-        $errors= [];
-    
-        if (hasEmptyField($data) >= 0) {
-            // It has an empty field. Echo an error and skip to next row
-             $errors+= ["Empty field is in row: $counter"];
-             continue;
+
+//     }
+
+    // echo $errors;
+
+
+// }
+
+    function studentValidation($data){
+        $userId = $data[0];
+        $password = $data[1];
+        $name = $data[2];
+        $school = $data[3];
+        $edollar = $data[4];
+
+        $errors = [];
+        $userDAO = new UserDAO();
+
+        $result = True;
+
+        if(strlen($userId)>128){
+            $result = False;
+            $error = "invalid userid";
+            $errors[] = $error; 
+        }
+        if($userDAO -> retrieveById($userId) != null) {
+            $result = False; 
+            $error = "duplicate userid";
+            $errors[] = $error; 
+        }
+        if(is_numeric($edollar) == False || $edollar < 0.0 || $edollar != round($edollar,2) ) {
+            $result = False;
+            $error = "invalid e-dollar";
+            $errors[] = $error; 
+        }
+        if(strlen($password)>128){
+            $result = False;
+            $error = "invalid password";
+            $errors[] = $error; 
+        }
+        if(strlen($name)>100){
+            $result = False;
+            $error = "invalid name";
+            $errors[] = $error; 
         }
 
+        // if error not null, delete row 
+        if($result == False) {
+            $sql="DELETE FROM users WHERE user_id = :userId";
+
+            $connMgr = new ConnectionManager();
+            $db = $connMgr->getConnection();
+
+            $query = $db->prepare($sql);
+            $query->setFetchMode(PDO::FETCH_ASSOC);
+            $query->bindParam(':userId', $userId, PDO::PARAM_STR);
+    
+            $query->execute();
+            $query->fetch(PDO::FETCH_ASSOC);
+        }
+
+        return $errors;   
     }
 
-    echo $errors;
-}
+    // To be completed
+    function courseValidation($data){
+        $course = $data[0];
+        $school = $data[1];
+        $title = $data[2];
+        $description = $data[3];
+        $examdate = $data[4];
+        $examstart = $data[5];
+        $examend = $data[6];
 
-// commonValidation($courses);
+        $errors = [];
+        $result = True; 
+        if(strlen($title)>100){
+            $result = False;
+            $error = "invalid title";
+            $errors[] = $error; 
+        }
+        
+    }
 ?>
