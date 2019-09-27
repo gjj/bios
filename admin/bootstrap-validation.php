f30a3065e2750bb07f1d994766e6b7c8047cefb8
edwinpangbohao <54808216+edwinpangbohao@users.noreply.github.com>
2019-09-27 18:27:29 +0800

    editied bootstrap validation
    included hasEmptyField function.
    debugged Bootstrap.php Course path

--- admin/bootstrap-validation.php
+++ admin/bootstrap-validation.php
@@ -1,11 +1,40 @@
 <?php
+
 require_once '../includes/common.php';
 // commonValidation to check each file, if there's a row with any of the column data missing, discard that row
+// incomplete
+function hasEmptyField(array $data)
+{
+    for ($i = 0; $i <=len($data); $i++) {
+        // Make sure that the key exists, isn't null or an empty string
+        if (!isset($data[$i]) || $data[$i] === '') {
+            
+            return $i;
+        }
+    }
+
+    return false;
+}
+// incomplete
 function commonValidation($file){
 
-    }
-}
+    $counter = 0;
 
+    while (($data = fgetcsv($file, 1000, ",")) !== FALSE) {        
+
+        $counter++;    
+        $errors= [];
+    
+        if (hasEmptyField($data) >= 0) {
+            // It has an empty field. Echo an error and skip to next row
+             $errors+= ["Empty field is in row: $counter"];
+             continue;
+        }
+
+    }
+
+    echo $errors;
+}
 
 // commonValidation($courses);
 ?>
