2745b3a5fb3739b92ab026a640befa06fd8079a0
alice <mesin.2017@sis.smu.edu.sg>
2019-09-27 17:12:54 +0800

    found error with fclose, resolved.

--- admin/bootstrap-validation.php
+++ admin/bootstrap-validation.php
@@ -1,3 +1,11 @@
 <?php
+require_once '../includes/common.php';
+// commonValidation to check each file, if there's a row with any of the column data missing, discard that row
+function commonValidation($file){
 
+    }
+}
+
+
+// commonValidation($courses);
 ?>
