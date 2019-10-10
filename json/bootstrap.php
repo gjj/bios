5e75fac80c509a2711ac88217bb8a7e8ed038888
Goi Jia Jian <jiajian.goi.2018@sis.smu.edu.sg>
2019-10-10 20:41:27 +0800

    test

--- json/bootstrap.php
+++ json/bootstrap.php
@@ -1,4 +1,20 @@
 <?php
+    require_once '../includes/common.php';
     require_once '../admin/bootstrap.php';
-    doBootstrap();
+    
+    $errors = [
+        isMissingOrEmpty('token')
+    ];
+
+    $errors = array_filter($errors);
+
+    if (!isEmpty($errors)) {
+        $result = [
+            "status" => "error",
+            "messages" => array_values($errors)
+        ];
+    }
+    else {
+        doBootstrap();        
+    }
 ?>
