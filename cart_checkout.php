e02e3870042db95f32e2b429ec92401fcc6f94f8
alice <mesin.2017@sis.smu.edu.sg>
2019-10-22 14:44:09 +0800

    updated to show errors, commented out repeated error msg

--- cart_checkout.php
+++ cart_checkout.php
@@ -40,9 +40,9 @@ if ($_POST) {
 
 
     // Validation: Make sure I sum(amount[]) < my current edollar!!!!
-    if ($sum > $user['edollar']) {
-        addError("You do not have enough edollar to place all your bids! Sum of all your bids: e\${$sum} vs. what you have: e\${$user['edollar']}.");
-    }
+    // if ($sum > $user['edollar']) {
+    //     addError("You do not have enough edollar to place all your bids! Sum of all your bids: e\${$sum} vs. what you have: e\${$user['edollar']}.");
+    // }
 
     // Validation: Make sure each bid is min. e$10.
     if (min($_POST['amount']) < 10) {
@@ -109,12 +109,7 @@ include 'includes/views/header.php';
                                 ?>
                                 <tr>
                                     <td>
-                                        <input type="number" name="amount[]" class="form-control" step="0.01"
-                                               min="<?php if ($currentRound['round'] == 2) {
-                                                   echo $minBidVal;
-                                               } else {
-                                                   echo 10;
-                                               } ?>"
+                                        <input type="number" name="amount[]" class="form-control" 
                                                required/>
                                     </td>
                                     <td><?php echo $cartItems['course']; ?><input type="hidden" id="course"
