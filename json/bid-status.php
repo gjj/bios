b133cd005617e9c4873bfb76207340c4690dcbf4
Goi Jia Jian <jiajian.goi.2018@sis.smu.edu.sg>
2019-11-01 00:45:12 +0800

    template for bid-status

---
+++ json/bid-status.php
@@ -0,0 +1,52 @@
+***REMOVED***
+require_once '../includes/common.php';
+
+header("Content-Type: application/json");
+$errors = [
+    isMissingOrEmpty('r'),
+    isMissingOrEmpty('token'), // Check which layer token validation is at!!
+];
+$errors = array_filter($errors);
+if (!$errors) {
+    $request = $_GET['r'];
+    $token = $_GET['token'];
+    $json = json_decode($request, true);
+    $errors = [
+        isMissingOrEmptyJson('course', $json),
+        isMissingOrEmptyJson('section', $json),
+    ];
+    $errors = array_filter($errors);
+    // If pass input validation...
+    if (!$errors) {
+        $course = $json['course'];
+        $section = $json['section'];
+
+        $courseDAO = new CourseDAO();
+        $sectionDAO = new SectionDAO();
+
+        if (!$courseDAO->retrieveByCode($course)) {
+            $errors[] = "invalid course";
+    ***REMOVED*** else if (!$sectionDAO->retrieveByCodeAndSection($course, $section)) {
+            $errors[] = "invalid section";
+    ***REMOVED***
+        $roundDAO = new RoundDAO();
+        $bidDAO = new BidDAO();
+        $currentRound = $roundDAO->getCurrentRound();
+
+        $bids = $bidDAO->retrieveAllBidsBySection($course, $section);
+***REMOVED***
+}
+if (!$errors) {
+    $result = [
+        "status" => "success",
+        "bids" => $bids
+    ];
+} else {
+    $result = [
+        "status" => "error",
+        "message" => array_values($errors)
+    ];
+}
+echo json_encode($result, JSON_PRETTY_PRINT | JSON_PRESERVE_ZERO_FRACTION);
+
+?>
